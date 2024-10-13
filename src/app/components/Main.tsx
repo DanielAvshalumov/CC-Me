4e8970591c3332f15c8ec602c5c65139d44e6c35
Daniel Avshalumov <avshalumov.daniel@gmail.com>
2024-10-13 15:14:57 -0400

    style change and job component

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Avatar, Box, List, ListItem, ListItemAvatar, ListItemText, Typography } from "@mui/material";
 import jsonData from "../public/json_samples/home_recents.json"
 
 const Main = () => {
@@ -8,9 +8,24 @@ const Main = () => {
         company: string;
         location: string;
         views: number;
-        ccs: number;}) => {
-
-    })
+        ccs: number;}) => 
+                <ListItem>
+                    <ListItemAvatar>
+                        <Avatar alt='construction worker' src='../public/images/avatar/construction-3384689_1280.jpg' />
+                    </ListItemAvatar>
+                    <ListItemText
+                        primary={
+                            <Typography variant="h6">
+                                {`${job.field}`}
+                            </Typography>
+                        }
+                        secondary={
+                        <Typography>
+                            <b>Company</b> - {`${job.company}\t`}<b>Views</b> - {`${job.views}`}<br />Location - {`${job.location}\t`}CC's - ${job.ccs}
+                        </Typography>}
+                    />
+                </ListItem>
+    )
 
     return(
         <Box display='flex' flexDirection='column' alignItems='center'>
@@ -18,6 +33,9 @@ const Main = () => {
             <Box display='flex' justifyContent='space-evenly' sx={{width:'100%'}} >
                 <Box display='flex' flexDirection='column'>
                     <Typography variant="h5">Jobs</Typography>
+                    <List>
+                        {jobElement}
+                    </List>
                 </Box>
                 <Box display='flex' flexDirection='column'>
                     <Typography variant="h5">Contractors</Typography>
--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -10,7 +10,7 @@ export default function Header(props : any) {
 
     return (
         <Container maxWidth="lg">
-                <AppBar position='static' sx={{ backgroundColor:'beige', boxShadow:'none', display:'flex', marginTop:'20px'}}>
+                <AppBar position='static' sx={{ backgroundColor:'beige', boxShadow:'none', display:'flex', marginTop:'20px',borderRadius:'30px', opacity:'60%', border: '4mm ridge rgba(211, 220, 50, .6)'}}>
                     <Toolbar>
                         <Typography variant="h3" sx={{ color:'black', marginRight:'30px'}}>CC'Me</Typography>
                         <Grid2 container spacing={3}>
