bf734af6ed3f0100f2d8fa09100ff30bdfc02a2e
Daniel Avshalumov <avshalumov.daniel@gmail.com>
2024-10-18 22:13:01 -0400

    more added to job component

--- src/app/contracts/page.tsx
+++ src/app/contracts/page.tsx
@@ -1,19 +1,23 @@
-import { Typography, Box, List } from "@mui/material";
+import { Typography, Box, List, Paper } from "@mui/material";
 import JobsComponent from "../components/Jobs";
+import JobDetails from "./JobDetails";
 
 const Contracts = () => {
 
 
     
     return (
-        <>
-            <Box display='flex' flexDirection='column'>
-                    <Typography variant="h5">Jobs</Typography>
-                    <List>
-                        <JobsComponent />
-                    </List>
-            </Box>
-        </>
+        <Box display='flex' style={{width:'85%', margin:'30px'}}>
+            <Paper elevation={0} sx={{marginRight:'auto'}}>
+                <Box display='flex' flexDirection='column' maxHeight={700} overflow='auto'>
+                        <Typography variant="h5">Jobs</Typography>
+                        <List>
+                            <JobsComponent />
+                        </List>
+                </Box>
+            </Paper>
+            <JobDetails />
+        </Box>
     )
 }
 
